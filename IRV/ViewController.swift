d361a58da02c918b1264b86f250aa6aaf3a7909f
Bill Anderson <billa@yahoo.com>
2016-11-20 08:52:43 -0800

    Initial Commit

---
+++ IRV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IRV
+//
+//  Created by Bill Anderson on 11/20/16.
+//  Copyright © 2016 Bill Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
