09edfa11cbe0ea4fa7437c45d4f80a76f81fee22
Bill Anderson <billa@yahoo.com>
2016-11-26 16:32:35 -0800

    Initial commit

--- IRV/IRVViewController.swift
+++ IRV/IRVViewController.swift
@@ -1,25 +1,39 @@
 //
-//  ViewController.swift
+//  IRVViewController.swift
 //  IRV
 //
-//  Created by Bill Anderson on 11/20/16.
+//  Created by Bill Anderson on 11/26/16.
 //  Copyright © 2016 Bill Anderson. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class IRVViewController: UIViewController {
+    
+    
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
 }
+
+
+
+
 
---
+++ IRV/VoterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  VoterViewController.swift
+//  IRV
+//
+//  Created by Bill Anderson on 11/26/16.
+//  Copyright © 2016 Bill Anderson. All rights reserved.
+//
+
+import UIKit
+
+class VoterViewController: IRVViewController {
+    
+    
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        title = "Voter"
+        
+        // Is user logged in?
+        
+        
+        
+    }
+    
+}
